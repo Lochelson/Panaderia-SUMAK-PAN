6363d809a66525ad0d3b6b182a7837036a1eedfd
Lochelson <lochelsontradicional@gmail.com>
2024-05-14 15:46:04 -0500

    Confirmacion Version 1

---
+++ src/logica/Main.java
@@ -0,0 +1,16 @@
+
+package logica;
+
+import Interfas.Iniciar_Secion;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Iniciar_Secion login = new Iniciar_Secion();
+        login.setVisible(true);
+        
+        
+        
+        
+    }
+}
